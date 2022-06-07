eb09e6ff5062fe9ce44ad1fc514612915b8f6149
AndersemReis <andersem.Reis@gmail.com>
2022-06-07 19:42:48 -0400

    trabalhando com strings

--- modulo01/aula10/strings.php
+++ modulo01/aula10/strings.php
@@ -3,11 +3,12 @@
 $nome = 'alessandro';
 $sobrenome = 'FEITOZA';
 
-$nome2 = 'Andersem';
+$nome2 = 'andersem';
 
 $sobrenome = strtolower($sobrenome);
 $nome = strtoupper($nome);
+$nome2 = ucfirst($nome2);
 
 echo $nome .PHP_EOL;
-
+echo $nome2.PHP_EOL;
 echo $sobrenome;
